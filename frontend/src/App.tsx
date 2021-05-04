c208e11826beb52ff2cfc13929d64ea598e8737d
brunopapait <brunohpapait@gmail.com>
2021-05-03 22:38:20 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <NavBar />
+      <div className='container'>
+        <h2 className='text-primary'>DevSuperior Pai</h2>
+      </div>
+      <Footer />
+    </>
   );
 }
 
